6d150841ec96219424cb04a79abedda53ac083c6
7Eddie20 <aditya.edn@gmail.com>
2025-07-08 10:38:06 +0530

    precision formatting

--- Basics/Data_Types/precision/precisionFormatting.cpp
+++ Basics/Data_Types/precision/precisionFormatting.cpp
@@ -25,8 +25,31 @@ int main()
     //default formatting
     cout << "floating point value in default formatting : " << f << endl;
     cout << "double floating point value in default formatting : " << d << endl;
+    cout << endl;
+    cout << endl;
 
     //fixed point
     cout << "floating point value in fixed point notation : " << fixed << f << endl;
     cout << "double floating point value in fixed point notation : " << fixed << d << endl;
+    cout << endl;
+    cout << endl;
+
+    //scientific notation
+    cout << "floating point value in scientific notation : " << scientific << f << endl;
+    cout << "double floating point value in scientific notation : " << scientific << d << endl;
+    cout << endl;
+    cout << endl;
+
+    //setprecisions
+    cout << "floating point value with set precision level 2 : " << fixed << setprecision(2) << f << endl;
+    cout << "floating point value with set precision level 5 : " << setprecision(5) << f << endl;
+    cout << "floating point value with set precision level 9 : " << setprecision(9) << f << endl;
+    cout << endl;
+    cout << endl;
+
+    cout << "double floating point value with set precision level 2 : " << setprecision(2) << d << endl;
+    cout << "double floating point value with set precision level 5 : " << setprecision(5) << d << endl;
+    cout << "double floating point value with set precision level 9 : " << setprecision(9) << d << endl;
+
+    return 0;
 }
